7ca3ba3ddc63f1c80ffb4be738dc801984aa717c
Rudá Moura <ruda.moura@gmail.com>
2012-05-03 00:03:50 -0300

    Initial import into Git.

---
+++ macspell.py
@@ -0,0 +1,300 @@
+#!/usr/bin/python -u
+# -*- coding: utf-8 -*-
+#
+# Spell checker designed on Cocoa's spell-checking facilities.
+#
+# Copyright (c) 2012 Rudá Moura
+#
+
+import os, sys, getopt, logging
+
+MACSPELL='@(#) International Ispell Version 3.1.20 (but really MacSpell 2012)'
+
+DICTIONARY_LIST = {
+    'american': 'en',
+    'brasileiro': 'pt_BR',
+    'british': 'en_GB',
+    'castellano': 'es',
+    'castellano8': 'es',
+    'dansk': 'da',
+    'default': 'en',
+    'deutsch': 'de',
+    'deutsch8': 'de',
+    'english': 'en',
+    'francais': 'fr',
+    'francais-tex': 'fr',
+    'francais7': 'fr',
+    'german': 'de',
+    'german8': 'de',
+    'italiano': 'it',
+    'nederlands': 'nl',
+    'nederlands8': 'nl',
+    'portugues': 'pt',
+    'russian': 'ru',
+    'russianw': 'ru',
+    'svenska': 'sv',
+}
+# FIXME: czech, esperanto, esperanto-tex, norsk, norsk7-tex, polish, slovak, slovenian, svenska
+
+Config = {
+    'LOG_LEVEL': logging.INFO,
+    'LOG_FILENAME': os.environ['HOME'] + '/Library/Logs/macspell.log',
+    'TERSE_MODE': False,
+    'AUTO_LANG': None,
+    'LANG': 'en',
+    'ENCODING': 'utf-8',
+}
+
+if os.environ.has_key('DEBUG'):
+    Config['LOG_LEVEL'] = logging.DEBUG
+
+logging.basicConfig(filename=Config['LOG_FILENAME'],
+                    level=Config['LOG_LEVEL'])
+logger = logging.getLogger('MacSpell')
+
+def get_line():
+    line = sys.stdin.readline()
+    logger.debug('Got line: %s' % line)
+    return unicode(line, Config['ENCODING'])
+
+def get_checker():
+    from Cocoa import NSSpellChecker
+    checker = NSSpellChecker.sharedSpellChecker()
+    logger.debug('Got checker: ' + str(checker))
+    return checker
+
+def check_spelling(checker, string, start=0):
+    from Cocoa import NSString
+    _string = NSString.stringWithString_(string)
+    _range, _count = checker.checkSpellingOfString_startingAt_language_wrap_inSpellDocumentWithTag_wordCount_(_string, start, None, False, 0, None)
+    logger.debug('Check spelling: %s range: %s count: %d' % (string.encode('utf-8'), _range, _count))
+    if _range.length == 0:
+        return True, _count, None, None
+    else:
+        word = string[_range.location:_range.location+_range.length]
+        logger.info('Misspelled word: ' + word.encode('utf-8'))
+        return False, _count, _range, word
+
+def guesses(checker, string, _range):
+    from Cocoa import NSString, NSRange
+    _string = NSString.stringWithString_(string)
+    _words = checker.guessesForWordRange_inString_language_inSpellDocumentWithTag_(_range, _string, None, 0)
+    n = len(_words)
+    words = u', '.join(_words)
+    logger.info('Guesses: ' + words)
+    return n, words
+
+def dict2lang(dictionary):
+    if DICTIONARY_LIST.has_key(dictionary):
+        Config['LANG'] = DICTIONARY_LIST[dictionary]
+    else:
+        logger.debug('Invalid dictionary: ' + dictionary)
+        print 'Invalid dictionary ' + dictionary
+        sys.exit(1)
+
+def set_language(checker, language):
+    if Config['AUTO_LANG'] == None:
+        Config['AUTO_LANG'] = checker.automaticallyIdentifiesLanguages()
+    checker.setAutomaticallyIdentifiesLanguages_(Config['AUTO_LANG'])
+    res = checker.setLanguage_(language)
+    if res:
+        logger.info('Selected language ' + language)
+    else:
+        logger.error('Invalid language: ' + language)
+        print 'Invalid language', language
+        sys.exit(1)
+
+def add_word(checker, word):
+    from Cocoa import NSString
+    _word = NSString.stringWithString_(word)
+    logger.info('Learning word ' + word)
+    checker.learnWord_(_word)
+
+def ignore_word(checker, word):
+    from Cocoa import NSString
+    _word = NSString.stringWithString_(word)
+    logger.info('Ignoring word ' + word)
+    checker.ignoreWord_inSpellDocumentWithTag_(word, 0)
+
+def remove_word(checker, word):
+    from Cocoa import NSString
+    _word = NSString.stringWithString_(word)
+    logger.info('Unlearning word ' + word)
+    checker.unlearnWord_(_word)
+
+def list_mode(checker):
+    logger.debug('Entered into List Mode')
+    logger.debug('Current language: ' + checker.language())
+    logger.debug('Current encoding: ' + Config['ENCODING'])
+    words = []
+    while True:
+        line = get_line()
+        if not line:
+            break
+        last = 0
+        while True:
+            ok, count, _range, word = check_spelling(checker, line, last)
+            if ok:
+                break
+            else:
+                last = _range.location + _range.length
+                words.append(word)
+    for word in words:
+        print word
+
+def pipe_mode(checker):
+    logger.debug('Entered into Pipe Mode')
+    logger.debug('Current language: ' + checker.language())
+    logger.debug('Current encoding: ' + Config['ENCODING'])
+    sys.stdout.write(MACSPELL + '\n')
+    while True:
+        line = get_line()
+        if len(line) == 0:
+            break
+        if line[0] == '!':
+            logger.debug('Enter terse mode')
+            Config['TERSE_MODE'] = True
+            continue
+        if line[0] == '%':
+            logger.debug('Exit terse mode')
+            Config['TERSE_MODE'] = False
+            continue
+        if line[0] == '+':
+            logger.debug('Enter Tex mode')
+            continue
+        if line[0] == '-':
+            logger.debug('Exit TeX mode')
+            continue
+        if line[0] == '*':
+            logger.debug('Add to personal dictionary')
+            add_word(checker, line[1:].lstrip())
+            continue
+        if line[0] == '@':
+            logger.debug('Accept word, but leave out of dictionary')
+            ignore_word(checker, line[1:].lstrip())
+            continue
+        if line.startswith('~nroff'):
+            continue
+        if line.startswith('~list'):
+            continue
+        if line.startswith('~tex'):
+            continue
+        if line.startswith('~plaintex'):
+            continue
+        if line.startswith('~latin1'):
+            Config['ENCODING'] = 'latin1'
+            continue
+        if line.startswith('~latin3'):
+            Config['ENCODING'] = 'latin3'
+            continue
+        if line[0] == '^':
+            logger.debug('Spell-check rest of line')
+        last = 0
+        while True:
+            ok, count, _range, word = check_spelling(checker, line, last)
+            if ok:
+                if Config['TERSE_MODE'] == False:
+                    for x in range(count):
+                        sys.stdout.write( '*\n' )
+                break
+            else:
+                last = _range.location + _range.length
+                n, words = guesses(checker, line, _range)
+                if Config['TERSE_MODE'] == False:
+                    for x in range(count-1):
+                        sys.stdout.write( '*\n' )
+                if n == 0:
+                    sys.stdout.write( '# ' + word.encode(Config['ENCODING']) + ' ' + str(_range.location) + '\n' )
+                else:
+                    sys.stdout.write( '& ' + word.encode(Config['ENCODING']) + ' %d %d:' % (n, _range.location) + ' ' + words.encode(Config['ENCODING']) + '\n' )
+        sys.stdout.write( '\n' )
+
+def usage(prog_name):
+    print '''Usage: %s [options] [command]
+Where [command] is one of:
+  -h|--help	display this help
+  -v|--version	display version
+  -l|--list	list mode, "ispell -l" compatibility mode
+  -a|--pipe	pipe mode, "ispell -a" compatibility mode
+  --list-dict	list all dictionaries available
+  --list-lang	list all languages available
+and [options] is any of the following:
+  -d|--dict=|--master=<dict>	name of the dictionary to use (english, american, brasileiro, etc.)
+  --lang=<code>		name of the language to use (en, en_US, pt_BR, etc.)
+  --encoding=<enc>	text encoding to use (utf-8, latin1, etc.)
+  --auto-lang=[yes|no]	automatically identify languages
+''' % prog_name
+
+def main(argv=None):
+    if argv == None:
+        argv = sys.argv
+    logger.debug('MacSpell started with arguments: ' + ', '.join(argv[1:]))
+    if os.environ.has_key('LANG'):
+        lang = os.environ['LANG']
+        if '.' in lang:
+            lang, enc = lang.split('.')
+            Config['ENCODING'] = enc.lower()
+        Config['LANG'] = lang
+    if os.environ.has_key('DICTIONARY'):
+        dict = os.environ['DICTIONARY']
+        dict2lang(dict)
+    try:
+        opts, args = getopt.getopt(argv[1:],
+                                   'vhalmBCd:D',
+                                   ('version', 'help', 'pipe', 'list', 'master=', 'dict=', 'lang=', 'encoding=', 'list-dict', 'list-lang', 'auto-lang='))
+    except getopt.error, msg:
+        print >> sys.stderr, msg[0]
+        return 2
+    enter_list = enter_pipe = False
+    for opt, arg in opts:
+        if opt in ('-v', '--version'):
+            print MACSPELL
+            return 0
+        if opt in ('-h', '--help'):
+            usage(sys.argv[0])
+            return 0
+        if opt in ('-l', '--list'):
+            enter_list = True
+        if opt in ('-a', '--pipe'):
+            enter_pipe = True
+        if opt in ('-m', '-B', '-C'):
+            pass
+        if opt in ('-d', '--dict', '--master'):
+            dict2lang(arg)
+        if opt == '--lang':
+            Config['LANG'] = arg
+        if opt == '--auto-lang':
+            if arg.lower() == 'yes':
+                Config['AUTO_LANG'] = True
+            elif arg.lower() == 'no':
+                Config['AUTO_LANG'] = False
+            else:
+                Config['AUTO_LANG'] = None
+        if opt == '--list-lang':
+            checker = get_checker()
+            dicts = checker.availableLanguages()
+            for d in dicts:
+                print d
+        if opt == '--list-dict':
+            dicts = DICTIONARY_LIST.keys()
+            dicts.sort()
+            for d in dicts:
+                print '%s (%s)' % (d, DICTIONARY_LIST[d])
+            return 0
+        if opt in ('--encoding'):
+            logger.debug('Set encoding to ' + arg)
+            Config['ENCODING'] = arg
+
+    if enter_list:
+        checker = get_checker()
+        set_language(checker, Config['LANG'])
+        list_mode(checker)
+    if enter_pipe:
+        checker = get_checker()
+        set_language(checker, Config['LANG'])
+        pipe_mode(checker)
+    logger.debug('Goodbye from MacSpell!')
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
